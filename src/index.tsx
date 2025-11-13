ddeec7637097de8b63017f3299e9f2247a13da82
rsojo <49877373+rsojo@users.noreply.github.com>
2025-11-12 19:03:38 -0500

    timeout aumentado

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { replaceGlobalFetch } from "./utils/fetchClient";
+
+// replace global fetch with a wrapper that applies default timeout
+replaceGlobalFetch();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
